3fe5a9c1793b39cb9a2bdc0f924a185d6ce9cb82
aagiorgitis <angios90@gmail.com>
2018-03-16 13:15:58 +0200

    implemented chess

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,43 +4,33 @@ import withAuthorization from '../Session/withAuthorization';
 import { db } from '../../firebase';
 
 class HomePage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      users: {}
-    };
-  }
-
-  componentDidMount() {
-    db.onceGetUsers().then(snapshot =>
-      this.setState(() => ({ users: snapshot.val() }))
-    );
-  }
 
   render() {
-    const { users } = this.state;
+
+      const enterGame = () => {
+          var frame = document.getElementById("startframe");
+          const token = document.getElementById("token").value;
+          frame.src = "http://chess.mdnbar.com/#/"+token;
+  }
 
     return (
       <div>
         <h1>Home</h1>
-        <p>The Home Page is accessible by every signed in user.</p>
+        <p>Create a new game using the button below, or enter a game token to enter an existing game.</p>
+              Enter game code here:  &nbsp;
+              <input id="token" type="text"></input>
+              <button onClick={enterGame}>Submit</button>
+          <div id="framediv">
+            <iframe id="startframe" src="http://chess.mdnbar.com/#/" height="500" width="700" style={{"border":"none"}}></iframe>
+         </div>
 
-        { !!users && <UserList users={users} /> }
       </div>
     );
   }
+
+
+
 }
-
-const UserList = ({ users }) =>
-  <div>
-    <h2>List of Usernames of Users</h2>
-    <p>(Saved in Firebase Database)</p>
-
-    {Object.keys(users).map(key =>
-      <div key={key}>{users[key].username}</div>
-    )}
-  </div>
 
 const authCondition = (authUser) => !!authUser;
 
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 const LandingPage = () =>
   <div>
     <h1>Landing</h1>
-    <p>The Landing Page is open to everyone, even if the user isn't signed in.</p>
+    <p>Online chess app using React for the view layer, Firebase for authentication
+    and the engine at chess.mdnbar.com for the chess application logic. </p>
+      <p><a href="https://github.com/Agios90/react-firebase-authentication">source on github</a></p>
   </div>
 
 export default LandingPage;
